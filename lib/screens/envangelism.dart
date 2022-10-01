fe34e20847e53450d59311152a47df68ebb20957
Moyinoluwa Fesobi <moyinoluwafesobi7@gmail.com>
2022-10-01 19:36:29 +0100

    Modify: Added Input fields

--- lib/screens/envangelism.dart
+++ lib/screens/envangelism.dart
@@ -11,68 +11,121 @@ class EnvangelismPage extends StatelessWidget {
         statusBarIconBrightness: Brightness.dark));
 
     return Scaffold(
-         body: SingleChildScrollView(
-             child: Container(
-         padding: const EdgeInsets.all(0),
+        body: SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            
-            
-
-             Container(
-             
-              decoration:  BoxDecoration(
+            Container(
+              decoration: BoxDecoration(
                 color: Color.fromRGBO(227, 228, 254, 1),
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                     
-                Container(
-              margin: const EdgeInsets.only(top: 24),
-              child: Center(
-                child: Image.asset(
-                  "assets/images/church_logo.png",
-                  fit: BoxFit.contain,
-                  width: 98,
-                  height: 40,
-                ),
-              ),
-            ),
-            Container(
-                margin: const EdgeInsets.only(top: 40, left: 10),
-                child: Row(
-                  children: [
-                    Text(
-                      "Evangelism",
-                      style: GoogleFonts.poppins(
-                          textStyle: const TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.w300,
-                              color: Color.fromRGBO(49, 83, 153, 1))),
+                  Container(
+                    margin: const EdgeInsets.only(top: 24),
+                    child: Center(
+                      child: Image.asset(
+                        "assets/images/church_logo.png",
+                        fit: BoxFit.contain,
+                        width: 98,
+                        height: 40,
+                      ),
                     ),
-                    Image.asset(
-                      "assets/images/user.png",
-                      width: 40,
-                      height: 40,
-                    )
-                  ],
-                )),
+                  ),
+                  Container(
+                      margin:
+                          const EdgeInsets.only(top: 40, left: 10, right: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Evangelism",
+                            style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w300,
+                                    color: Color.fromRGBO(49, 83, 153, 1))),
+                          ),
+                          Image.asset(
+                            "assets/images/user.png",
+                            width: 40,
+                            height: 40,
+                          )
+                        ],
+                      )),
+                ],
+              ),
+              
+
+            ),
+
+            Container(
+               margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+               
+                children: <Widget>[
+                    TextFormField(
+                      style: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                      cursorColor: Color.fromRGBO(42, 42, 42, 1),
+                     decoration: const InputDecoration(
+                       labelText: "Date",
+                       labelStyle: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                       focusedBorder: UnderlineInputBorder(
+                         borderSide: BorderSide(
+                           color: Color.fromRGBO(49, 83, 153, 1),
+                           width: 1,
+                         ),
+                       ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromRGBO(42, 42, 42, 1)
+                          )
+                        )
+                     )
+                                     ),
 
 
 
-                    ],
+                                      TextFormField(
+                     decoration: const InputDecoration(
+                       labelText: "Location",
+                       border: UnderlineInputBorder(
+                         
+                       ),
+                     )
+                                     )
+                ],
               ),
             ),
 
+            Container(
+               margin: const EdgeInsets.only(left: 20, right: 20, top: 24),
+               child: Row(
+                 children: [
+                   Text("Attendants", style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                                  color: Color.fromRGBO(49, 83, 153, 1),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,),
+                              
+                   )
+                   ),
+                  
+
+                 ],
+               )
+
+            )
+          ],
 
 
 
-
-             ],
         ),
       ),
-           ));
+    ));
   }
 }
