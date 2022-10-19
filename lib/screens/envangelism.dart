ed359cf468e16d6fb734b325c3df01e6e1b31fa2
Moyinoluwa Fesobi <moyinoluwafesobi7@gmail.com>
2022-10-19 09:29:22 +0100

    Modify: Added titles, input fields

--- lib/screens/envangelism.dart
+++ lib/screens/envangelism.dart
@@ -37,7 +37,7 @@ class EnvangelismPage extends StatelessWidget {
                   ),
                   Container(
                       margin:
-                          const EdgeInsets.only(top: 40, left: 10, right: 10),
+                          const EdgeInsets.only(top: 40, left: 10, right: 10, bottom: 10),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
@@ -58,72 +58,268 @@ class EnvangelismPage extends StatelessWidget {
                       )),
                 ],
               ),
-              
-
             ),
-
             Container(
-               margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-               
                 children: <Widget>[
-                    TextFormField(
+                  TextFormField(
                       style: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
                       cursorColor: Color.fromRGBO(42, 42, 42, 1),
-                     decoration: const InputDecoration(
-                       labelText: "Date",
-                       labelStyle: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
-                       focusedBorder: UnderlineInputBorder(
-                         borderSide: BorderSide(
-                           color: Color.fromRGBO(49, 83, 153, 1),
-                           width: 1,
-                         ),
-                       ),
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color.fromRGBO(42, 42, 42, 1)
-                          )
-                        )
-                     )
-                                     ),
+                      decoration: const InputDecoration(
+                          labelText: "Date",
+                          labelStyle:
+                              TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(49, 83, 153, 1),
+                              width: 1,
+                            ),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromRGBO(42, 42, 42, 1))))),
+                  TextFormField(
+                      style: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                      cursorColor: Color.fromRGBO(42, 42, 42, 1),
+                      decoration: const InputDecoration(
+                          labelText: "Location",
+                          labelStyle:
+                              TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(49, 83, 153, 1),
+                              width: 1,
+                            ),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromRGBO(42, 42, 42, 1))))),
+                ],
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, top: 24),
+                child: Row(
+                  children: [
+                    Text("Attendants",
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                            color: Color.fromRGBO(49, 83, 153, 1),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )),
+                  ],
+                )),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TextFormField(
+                        style: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                        cursorColor: Color.fromRGBO(42, 42, 42, 1),
+                        decoration: const InputDecoration(
+                            labelText: "Fullname",
+                            labelStyle:
+                                TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(49, 83, 153, 1),
+                                width: 1,
+                              ),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(42, 42, 42, 1))))),
+                    TextFormField(
+                        style: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                        cursorColor: Color.fromRGBO(42, 42, 42, 1),
+                        decoration: const InputDecoration(
+                            labelText: "Fullname",
+                            labelStyle:
+                                TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(49, 83, 153, 1),
+                                width: 1,
+                              ),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(42, 42, 42, 1))))),
+                    TextFormField(
+                        style: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                        cursorColor: Color.fromRGBO(42, 42, 42, 1),
+                        decoration: const InputDecoration(
+                            labelText: "Fullname",
+                            labelStyle:
+                                TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(49, 83, 153, 1),
+                                width: 1,
+                              ),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(42, 42, 42, 1))))),
+                  ]),
+            ),
+            Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, top: 24),
+                child: Row(
+                  children: [
+                    Text("Comments",
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                            color: Color.fromRGBO(49, 83, 153, 1),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )),
+                  ],
+                )),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TextFormField(
+                        style: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                        cursorColor: Color.fromRGBO(42, 42, 42, 1),
+                        decoration: const InputDecoration(
+                            labelText: "Type your comments here",
+                            labelStyle:
+                                TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(49, 83, 153, 1),
+                                width: 1,
+                              ),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(42, 42, 42, 1))))),
+                  ]),
+            ),
+            Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, top: 24),
+                child: Row(
+                  children: [
+                    Text("Leader",
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                            color: Color.fromRGBO(49, 83, 153, 1),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )),
+                  ],
+                )),
+            Container(
+              margin: const EdgeInsets.only(
+                  left: 20, right: 20, top: 10, bottom: 20),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TextFormField(
+                        style: TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                        cursorColor: Color.fromRGBO(42, 42, 42, 1),
+                        decoration: const InputDecoration(
+                            labelText: "Led by",
+                            labelStyle:
+                                TextStyle(color: Color.fromRGBO(42, 42, 42, 1)),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromRGBO(49, 83, 153, 1),
+                                width: 1,
+                              ),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(42, 42, 42, 1))))),
+                  ]),
+            ),
 
 
+            Container(
+              margin: const EdgeInsets.only(
+                  left: 20, right: 20, top: 10, bottom: 20),
 
-                                      TextFormField(
-                     decoration: const InputDecoration(
-                       labelText: "Location",
-                       border: UnderlineInputBorder(
-                         
-                       ),
-                     )
-                                     )
+                     padding: const EdgeInsets.all(10),
+                       height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                      ),
+              child:Center(
+                child: Text(
+                          "+",
+                          style: GoogleFonts.poppins(
+                              textStyle: const TextStyle(
+                                  color: Color.fromRGBO(49, 83, 153, 1),
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w400)),
+                        ),
+              )
+            ),
+           
+
+
+               Container(
+              margin: const EdgeInsets.only(left: 20, top: 24, bottom: 20,),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.all(1),
+                      padding: const EdgeInsets.all(10),
+                      height: 52,
+                      width: 156,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Color.fromRGBO(49, 83, 153, 1),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Save",
+                          style: GoogleFonts.poppins(
+                              textStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400)),
+                        ),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.only(
+                          top: 1, bottom: 1, left: 10, right: 1),
+                      padding: const EdgeInsets.all(10),
+                      height: 52,
+                      width: 156,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                       border: Border.all(width: 1, color: Color.fromRGBO(49, 83, 153, 1)),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Cancel",
+                          style: GoogleFonts.poppins(
+                              textStyle: const TextStyle(
+                                  color: Color.fromRGBO(49, 83, 153, 1),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400)),
+                        ),
+                      ))
                 ],
               ),
             ),
 
-            Container(
-               margin: const EdgeInsets.only(left: 20, right: 20, top: 24),
-               child: Row(
-                 children: [
-                   Text("Attendants", style: GoogleFonts.poppins(
-                      textStyle: const TextStyle(
-                                  color: Color.fromRGBO(49, 83, 153, 1),
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,),
-                              
-                   )
-                   ),
-                  
 
-                 ],
-               )
-
-            )
           ],
-
-
-
         ),
       ),
     ));
